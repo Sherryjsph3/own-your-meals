0d87e045abd3e71a5733088c8605c6838dad3ed2
Sherry Joseph <sherryjoseph@sherrys-air.myfiosgateway.com>
2021-06-14 21:37:08 -0400

    wip: recipeSeed data

---
+++ models/recipeSeed.js
@@ -0,0 +1,283 @@
+module.exports = [
+    {
+        name: 'Avocado Toast with Poached Eggs',
+        image: 'https://i.imgur.com/et44oEe.png',
+        time: '30 minutes',
+        ingredients: [
+            'bread',
+            '2 cup guacamole',
+            '6 eggs',
+            'Pepper',
+            'Crushed red pepper',
+            'White rice vinegar',
+            'Extra virgin olive oil'
+        ],
+        description: 'The best, healthy, quick and easy lunch or breakfast avocado egg toast, homemade with simple ingredients in one pan in 10 minutes. Creamy mashed avocados are spread on crispy toasted bread to make an open sandwich'
+    },
+    {
+        name: 'Mushroom Omelette',
+        image: 'https://i.imgur.com/MdWiQ1P.jpg',
+        time: '15 minutes',
+        ingredients: [
+            '5 Mushrooms',
+            '1 Spring onion',
+            '2 Large Eggs',
+            'Gruyere Cheese',
+            'Spinach'   
+        ],
+        description: 'Mushroom Omelette with gruyere cheese - the ideal breakfast or brunch for mushroom lovers. Sauteed mushrooms and shallots, shredded gruyere cheese and chopped chives are folded into the perfect omelette in this healthy and easy'
+    },
+    {
+        name: 'Banana Walnut Overnight Oats',
+        image: 'https://i.imgur.com/mn1JfcB.png',
+        time: '10 minutes',
+        ingredients: [
+            '2 Medium Bananas',
+            '2 cups Rolled oats',
+            '1 tsp Vanilla extract',
+            '4 tsp Chia seeds',
+            '1/4 cup Walnuts'   
+        ],
+        description: 'Banana Nut Overnight Oats. Pop them in the fridge overnight for an easy, healthy and nutritious breakfast waiting for you in the morning'
+    },
+    {
+        name: 'Smoked Salmon + Scrambled Eggs',
+        image: 'https://i.imgur.com/SqQtINH.jpg',
+        time: '13 minutes',
+        ingredients: [
+            '7 oz Smoked salmon',
+            '2 Microgreen',
+            '1 tbsp Scallions',
+            '2 Large Eggs',
+            '2 slices Rye Toast'  
+        ],
+        description: 'This Smoked Salmon + Poached Eggs on Toast brings at-home brunch to a whole new level. Skip the pancakes next weekend and sink your utensils into this'
+    },
+    {
+        name: 'Black Bean Breakfast Burrito',
+        image: 'https://i.imgur.com/lVpWvb9.jpg',
+        time: '25 minutes',
+        ingredients: [
+            '1 (15 ounce) can Black beans',
+            '8-inch whole wheat tortillas',
+            '1/2 tsp Cumin',
+            '2 Large Eggs',
+            '1/4 tsp Chili powder',
+            '1 Red bell pepper',
+            '1/4 Red onion',  
+        ],
+        description: 'These healthy breakfast burrito recipes are loaded with vegetables, lean proteins, and wrapped up in a tortilla for on-the-go eating. Get the recipes here'
+    },
+    {
+        name: 'Hawaiian ',
+        image: 'https://i.imgur.com/wY0fh2J.jpg',
+        time: '20 minutes',
+        ingredients: [
+            '1 Banana', 
+            '1 Bananas',
+            '1 cup Berries',
+            '1 Granola',
+            '1 Chia seeds',
+            '1 Coconut flakes'
+        ],
+        description: 'This refreshing and delicious homemade Hawaiian acai bowl is the perfect healthy breakfast and it is easy to make! Top with fresh fruit, nuts, and more'
+    },
+    {
+        name: 'Whole Weat Protein Pancakes',
+        image: 'https://i.imgur.com/S2CRg5o.jpg',
+        time: '28 minutes',
+        ingredients: [
+            '1/2 cup Whole-wheat flour',
+            '1/2 cup Vanilla protein powder',
+            '2 eggs',
+            '1 medium Banana',
+            '1 tsp Vanilla extract'  
+        ],
+        description: 'These yummy whole wheat pancakes made with cottage cheese are packed with protein. A satisfying way to start your day'
+    },
+    {
+        name: 'Purple Chicken Tacos',
+        image: 'https://i.imgur.com/BibXWPW.jpg',
+        time: '45 minutes',
+        ingredients: [
+            '1 Hand full Cilantro',
+            '1 Feta cheese',
+            '1 1/2 cup Purple Corn flour',
+            '1 cup Tapioca flour',
+            '1 1/2 lbs Chicken Breast',
+        ],
+        description: 'If you are a fan of Mexican food you are going to loooooove these. Our Organic Purple Corn Flour makes the most amazing soft tacos that are super good for you too (you can read all about that here)'
+    },
+    {
+        name: 'Pumpkin Soup',
+        image: 'https://i.imgur.com/skuz46B.jpg',
+        time: '20 minutes',
+        ingredients: [
+            '3 cloves Garlic',
+            '1/8 tsp Ginger, ground', 
+            '1 (15-ounce) can Pumpkin puree',
+            '2 tbsp Thyme, fresh leaves',
+            '1 Yellow onion, medium'   
+        ],
+        description: 'The easiest (and tastiest) pumpkin soup, thanks to a can of pumpkin and your spice drawer.Transfer vegetables to a large pot over medium heat and add stock. Use an immersion blender to puree until smooth'
+    },
+    {
+        name: 'Jacket Sweet Potato & Guacamole',
+        image: 'https://i.imgur.com/wGlXCPS.png',
+        time: '50 minutes',
+        ingredients: [
+            '1 bunch Spring onion',
+            '1 large Sweet potatoes',
+            '1 cup Guacamole',
+            '1/4 cup pomegranate'   
+        ],
+        description: 'These sweet potato jackets with guacamole and black beans are super easy, fun to make and of course super nutritious. There’s not peeling involved and no standing in the kitchen for too long'
+    },
+    {
+        name: 'Fruit & Vegetable Smoothie',
+        image: 'https://i.imgur.com/D7464Q8.jpg',
+        time: '8 minutes',
+        ingredients: [
+            '1 Apple, green',
+            '1 stalk Celery',
+            '1/2 Cucumber',
+            '1 Ginger, fresh',
+            '1/2 cup Kale',
+            '1 Lime',
+            '1/2 cup Spinach' 
+        ],
+        description: 'Whip up this fruit and vegetable smoothie to start your day with plant powered nutrients. Perfect as a nutrient-packed breakfast, or a refreshing mid-morning pick me up'
+    },
+    {
+        name: 'Tofu Summer Salad',
+        image: 'https://i.imgur.com/BHtJs14.png',
+        time: '12 minutes',
+        ingredients: [
+            '3 cups Cucumber',
+            '1/2 Lemon',
+            '1/2 cup Mint, fresh',
+            '1/3 cup Red onion',
+            '250 g Tofu'  
+        ],
+        description: 'TThis is one healthy salad with a super refreshing salad dressing recipe made with yuzu juice and honey. If you are a fan of yuzu, you definitely do not want to miss this'
+    },
+    {
+        name: 'Chickpea Sandwich',
+        image: 'https://i.imgur.com/qZ4qV5p.png',
+        time: '15 minutes',
+        ingredients: [
+            '1 Avocado',
+            '1 cup or arugula',
+            '1 (15 ounce) can Chickpeas',
+            '2 tbsp red onion',
+            '1 Tomato, large'
+        ],
+        description: 'Buffalo Chickpea Sandwich | low calorie vegan sandwich recipes | vegan avocado sandwich recipes See more great meal planning ideas for vegan sandwiches'
+    },
+    {
+        name: 'Oven Roasted Chicken and Vegetables',
+        image: 'https://i.imgur.com/4cLmIMz.png',
+        time: '30 minutes',
+        ingredients: [
+            '1 1/2 lbs Chicken breasts',
+            '2 (8-ounce) Baby bell peppers, mixed',
+            '1 Mango',
+            '1/2 Mixed Vegetables',
+            '1/2 Brocoli'  
+        ],
+        description: 'The best, healthy, low carb, quick and easy oven roasted chicken and veggies, homemade with simple ingredients in sheet pan or one pan in 30 minutes. Loaded with Italian flavors'
+    },
+    {
+        name: 'Purple Chicken Tacos',
+        image: 'https://i.imgur.com/BibXWPW.jpg',
+        time: '45 minutes',
+        ingredients: [
+            '1 Hand full Cilantro',
+            '1 Feta cheese',
+            '1 1/2 cup Purple Corn flour',
+            '1 cup Tapioca flour',
+            '1 1/2 lbs Chicken Breast',
+        ],
+        description: 'If you are a fan of Mexican food you are going to loooooove these. Our Organic Purple Corn Flour makes the most amazing soft tacos that are super good for you too (you can read all about that here)'
+    },
+    {
+        name: 'Pumpkin Soup',
+        image: 'https://i.imgur.com/skuz46B.jpg',
+        time: '20 minutes',
+        ingredients: [
+            '3 cloves Garlic',
+            '1/8 tsp Ginger, ground', 
+            '1 (15-ounce) can Pumpkin puree',
+            '2 tbsp Thyme, fresh leaves',
+            '1 Yellow onion, medium'   
+        ],
+        description: 'The easiest (and tastiest) pumpkin soup, thanks to a can of pumpkin and your spice drawer.Transfer vegetables to a large pot over medium heat and add stock. Use an immersion blender to puree until smooth'
+    },
+    {
+        name: 'Jacket Sweet Potato & Guacamole',
+        image: 'https://i.imgur.com/wGlXCPS.png',
+        time: '50 minutes',
+        ingredients: [
+            '1 bunch Spring onion',
+            '1 large Sweet potatoes',
+            '1 cup Guacamole',
+            '1/4 cup pomegranate'   
+        ],
+        description: 'These sweet potato jackets with guacamole and black beans are super easy, fun to make and of course super nutritious. There’s not peeling involved and no standing in the kitchen for too long'
+    },
+    {
+        name: 'Fruit & Vegetable Smoothie',
+        image: 'https://i.imgur.com/D7464Q8.jpg',
+        time: '8 minutes',
+        ingredients: [
+            '1 Apple, green',
+            '1 stalk Celery',
+            '1/2 Cucumber',
+            '1 Ginger, fresh',
+            '1/2 cup Kale',
+            '1 Lime',
+            '1/2 cup Spinach' 
+        ],
+        description: 'Whip up this fruit and vegetable smoothie to start your day with plant powered nutrients. Perfect as a nutrient-packed breakfast, or a refreshing mid-morning pick me up'
+    },
+    {
+        name: 'Tofu Summer Salad',
+        image: 'https://i.imgur.com/BHtJs14.png',
+        time: '12 minutes',
+        ingredients: [
+            '3 cups Cucumber',
+            '1/2 Lemon',
+            '1/2 cup Mint, fresh',
+            '1/3 cup Red onion',
+            '250 g Tofu'  
+        ],
+        description: 'TThis is one healthy salad with a super refreshing salad dressing recipe made with yuzu juice and honey. If you are a fan of yuzu, you definitely do not want to miss this'
+    },
+    {
+        name: 'Chickpea Sandwich',
+        image: 'https://i.imgur.com/qZ4qV5p.png',
+        time: '15 minutes',
+        ingredients: [
+            '1 Avocado',
+            '1 cup or arugula',
+            '1 (15 ounce) can Chickpeas',
+            '2 tbsp red onion',
+            '1 Tomato, large'
+        ],
+        description: 'Buffalo Chickpea Sandwich | low calorie vegan sandwich recipes | vegan avocado sandwich recipes See more great meal planning ideas for vegan sandwiches'
+    },
+    {
+        name: 'Oven Roasted Chicken and Vegetables',
+        image: 'https://i.imgur.com/4cLmIMz.png',
+        time: '30 minutes',
+        ingredients: [
+            '1 1/2 lbs Chicken breasts',
+            '2 (8-ounce) Baby bell peppers, mixed',
+            '1 Mango',
+            '1/2 Mixed Vegetables',
+            '1/2 Brocoli'  
+        ],
+        description: 'The best, healthy, low carb, quick and easy oven roasted chicken and veggies, homemade with simple ingredients in sheet pan or one pan in 30 minutes. Loaded with Italian flavors'
+    },
+    
+]
